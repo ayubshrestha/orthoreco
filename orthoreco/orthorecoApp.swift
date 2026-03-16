f36feb3fd927a8fae96216e4c74e0e9e7b8215ab
shrestha.ayub09 <shrestha.ayub09@gmail.com>
2026-03-16 15:36:52 +0800

    Initial Commit

---
+++ orthoreco/orthorecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  orthorecoApp.swift
+//  orthoreco
+//
+//  Created by Ayub Shrestha on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct orthorecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
